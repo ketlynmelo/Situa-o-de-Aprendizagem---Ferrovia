ea582fc72bb659b6a32d5f79ce1a156071c523ee
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-09-04 09:30:43 -0300

    ADD - Adicionei a conexao ao banco de dados

---
+++ public/infra/conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "situação_aprendizagem";
+
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+    if ($conn->connect_error){
+        die ("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    };
+
+    $conn->set_charset("utf8mb4");
+
+
+
+
+
+?>
